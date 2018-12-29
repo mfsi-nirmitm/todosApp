8b171d8ba9322f605a789257ba4744a78a4e170e
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-12-29 07:42:47 +0530

    add todo and update todo

--- restfull-web-services/src/main/java/com/todos/rest/webservices/restfullwebservices/todo/TodoResource.java
+++ restfull-web-services/src/main/java/com/todos/rest/webservices/restfullwebservices/todo/TodoResource.java
@@ -1,14 +1,20 @@
 package com.todos.rest.webservices.restfullwebservices.todo;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
@@ -39,5 +45,29 @@ public class TodoResource {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username,
+			@PathVariable long id,
+			@RequestBody Todo todo
+	) {
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
+	
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Void> addTodo(
+			@PathVariable String username,
+			@RequestBody Todo todo
+	) {
+		Todo createdTodo = todoService.save(todo); 
+		
+		// get current resource url
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
